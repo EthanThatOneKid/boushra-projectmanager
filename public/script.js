e298744c94e4f1623565eca0c08c8f4debb6a8a6
Boushra Bettir <116927138+boushrabettir@users.noreply.github.com>
2023-02-04 15:40:58 -0800

    Fixed JSON Bug + Data Retrival.
    Had a bug where when I submitted a form, it got pushed to both json files for both employees and projects.
    
    Forms for both add employees and projects are created and do a  post request to the server side.
    
    Implemented some basic CSS which I hate.
    Responsive design is still yet to come.

--- public/script.js
+++ public/script.js
@@ -3,6 +3,9 @@
 let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay');
 let add = document.querySelector('.add');
+let employeeOverlay = document.querySelector('.employeeOverlay');
+let projectForm = document.querySelector('form');
+
 add.addEventListener('click', () => {
   modal.classList.toggle('hidden');
   overlay.classList.toggle('hidden');
@@ -13,7 +16,13 @@ const CloseModule = () => {
   overlay.classList.toggle('hidden');
 };
 
+const CloseEmployeeModule = () => {
+  employeeModal.classList.toggle('hidden');
+  employeeOverlay.classList.toggle('hidden');
+};
+
 overlay.addEventListener('click', CloseModule);
+employeeOverlay.addEventListener('click', CloseEmployeeModule);
 
 document.addEventListener('keydown', (e) => {
   if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
@@ -21,36 +30,87 @@ document.addEventListener('keydown', (e) => {
   }
 });
 
+/* Adding Employee Btns */
+let employeeBtn = document.querySelector('.add-employee-btn');
+let employeeForm = document.querySelector('.form-employee');
+let employeeModal = document.querySelector('.employee');
+let total = document.querySelector('span');
+
+employeeBtn.addEventListener('click', () => {
+  employeeModal.classList.toggle('hidden');
+  employeeOverlay.classList.toggle('hidden');
+});
+
 /* Attempting to use JSON before using MongoDB */
 /* Sending a POST request for the backend to use */
 
-let form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-  /* I want to refresh everytime they make a new project! */
-  /* This also works then the inputs will be refreshed as well */
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData);
-  console.log(data);
+let dataArray = [];
+let formData1;
+employeeForm.addEventListener('submit', (e) => {
+  let formData1 = new FormData(employeeForm);
+  let data = Object.fromEntries(formData1);
+  //dataArray.push(data);
 
-  fetch('http://localhost:3000', {
+  fetch('http://localhost:3000/employee', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(data).toLowerCase(),
   });
 
   console.log('Button clicked');
 });
 
-/* Adding their own pages */
-let projects = document.querySelectorAll('.projects');
-projects.forEach((project, index) => {
-  project.addEventListener('click', () => {
-    fetch('/projects.json')
-      .then((response) => response.json())
-      .then((d) => {
-        console.log(`${d[index].project} was clicked.`);
-      });
+projectForm.addEventListener('submit', (e) => {
+  const formData = new FormData(projectForm);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  fetch('http://localhost:3000/project', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data).toLowerCase(),
   });
+  console.log('Button clicked');
 });
+
+/* This will be for the search bar */
+
+/*
+If the input/ value of the search bar is equivalent to one of the names letters
+ */
+// let arraySearch = Array.of(search.value);
+
+let search = document.querySelector('.search-bar');
+fetch('./employees.json')
+  .then((response) => response.json())
+  .then((data) => {
+    dataArray = data.employee;
+    // if (!dataArray) return;
+    search.addEventListener('input', (e) => {
+      let searching = search.value.toLowerCase();
+      console.log(searching);
+      // let filteredData = dataArray.filter((element) => {
+      //   return Object.values(element).some((value) =>
+      //     value.includes(searching)
+      //   );
+      // });
+      console.log(dataArray);
+      console.log(filteredData);
+    });
+  });
+
+// /* Adding their own pages */
+// let projects = document.querySelectorAll('.projects');
+// projects.forEach((project, index) => {
+//   project.addEventListener('click', () => {
+//     fetch('/projects.json')
+//       .then((response) => response.json())
+//       .then((d) => {
+//         console.log(`${d[index].project} was clicked.`);
+//       });
+//   });
+// });
--- server.js
+++ server.js
@@ -22,16 +22,17 @@ app.use(express.static('public'));
 // }
 
 // ConnectionMongo();
+let renderingProjects = [];
+let renderingEmployees = [];
 app.listen(3000);
+
 app.get('/', (req, res) => {
   /* We are reading the contents and then rendering all to the front end */
-  let renderingProjects = [];
-  let renderingEmployees = [];
   try {
     renderingProjects = JSON.parse(fs.readFileSync('./public/projects.json'));
     renderingEmployees = JSON.parse(fs.readFileSync('./public/employees.json'));
   } catch (err) {
-    console.error(err);
+    console.log(err);
   }
   res.render('index', {
     title: 'Homepage',
@@ -40,28 +41,47 @@ app.get('/', (req, res) => {
   });
 });
 
-app.post('/', (req, res) => {
-  /* Looks at the post request and then parses it, reads file, pushes it, and then writes it into the projects.json */
-  let all = [];
+app.post('/employee', (req, res) => {
+  /* Looks at the post request and then parses it, reads file, pushes it, and then writes it into the employees.json */
   try {
-    /* Reading file first in order to create a new updated Project, (looking at the contents) */
-    all = JSON.parse(fs.readFileSync('./public/projects.json'));
+    /* Reading file first in order to create a new updated Employee, (looking at the contents) */
+    renderingEmployees = JSON.parse(fs.readFileSync('./public/employees.json'));
   } catch (err) {
     console.error(err);
   }
-  all.push(req.body);
-  fs.writeFileSync('./public/projects.json', JSON.stringify(all), 'utf-8');
+  renderingEmployees.push(req.body);
+  fs.writeFileSync(
+    './public/employees.json',
+    JSON.stringify(renderingEmployees),
+    'utf-8'
+  );
 });
 
-app.get(`/`, async (req, res) => {
-  const projectname = req.params.projectname;
-  fetch('./public/projects.json')
-    .then((response) => response.json())
-    .then((d) => {
-      const project = d.find((p) => p.project === projectname);
-      if (!project) {
-        return res.sendStatus(404);
-      }
-      res.render('projects', { project });
-    });
+app.post('/project', (req, res) => {
+  /* Looks at the post request and then parses it, reads file, pushes it, and then writes it into the projects.json */
+  try {
+    /* Reading file first in order to create a new updated Project, (looking at the contents) */
+    renderingProjects = JSON.parse(fs.readFileSync('./public/projects.json'));
+  } catch (err) {
+    console.error(err);
+  }
+  renderingProjects.push(req.body);
+  fs.writeFileSync(
+    './public/projects.json',
+    JSON.stringify(renderingProjects),
+    'utf-8'
+  );
 });
+
+// app.get(`/`, async (req, res) => {
+//   const projectname = req.params.projectname;
+//   fetch('./public/projects.json')
+//     .then((response) => response.json())
+//     .then((d) => {
+//       const project = d.find((p) => p.project === projectname);
+//       if (!project) {
+//         return res.sendStatus(404);
+//       }
+//       res.render('projects', { project });
+//     });
+// });
